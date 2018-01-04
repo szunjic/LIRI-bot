749e77437609a8d8910600b0d0c4bff51ba9b4f2
Sofia <sz14g@my.fsu.edu>
2018-01-04 10:55:27 -0600

    final logic working

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('keys loaded');
+
+var twitterKeys = {
+  consumer_key: 'v9nbvbagL1BK7NinYNk5GG6ss',
+  consumer_secret: 'Ny2iR6Rxutbc2xsy8nqn1gtmhEiRWizQbvlKRo6VeFUsZVzS8r',
+  access_token_key: '836293438899367936-VfXGG2czJgHzV6BOuoh2AWaWuAKaUaf',
+  access_token_secret: '9xvgDZS6tArZBzxzUS69xObWh8lvKtXLaAsHiXOp8kPuG',
+}
+
+module.exports = twitterKeys;
